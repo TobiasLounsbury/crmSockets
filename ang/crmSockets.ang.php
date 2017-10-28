cc0abf9ca20f59ab192f828433ccdb306ead8097
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2017-10-28 13:03:29 -0700

    Added empty civix module template

---
+++ ang/crmSockets.ang.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+  'js' =>
+    array(
+      0 => 'ang/crmSockets.js',
+      1 => 'ang/crmSockets/*.js',
+      2 => 'ang/crmSockets/*/*.js'
+    ),
+  'partials' => array(0 => 'ang/crmSockets'),
+  'settings' => array(),
+  'requires' => array('crmUtil')
+);
---
+++ ang/crmSockets.js
@@ -0,0 +1,4 @@
+(function(angular, $, _) {
+  // Declare a list of dependencies.
+  angular.module('crmSockets', CRM.angRequires('crmSockets'));
+})(angular, CRM.$, CRM._);
